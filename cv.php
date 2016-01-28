005b1e649687b575c442a3fc6dcde119967633d1
Jean-Alexandre Barszcz <jalex_b@hotmail.com>
2016-01-28 18:24:55 -0500

    simple cv.php access logging

--- cv.php
+++ cv.php
@@ -1,4 +1,39 @@
 <?php
+// --------------LOGGING--------------------
+
+//ASSIGN VARIABLES TO USER INFO
+$time = date("M j G:i:s Y");
+$ip = getenv('REMOTE_ADDR');
+$host = $_SERVER['REMOTE_HOST'];
+$userAgent = getenv('HTTP_USER_AGENT');
+$referrer = $_SERVER['HTTP_REFERER'];
+$query = $_SERVER['REQUEST_URI'];
+
+//COMBINE VARS INTO OUR LOG ENTRY
+$msg = "[" . $time . "] (" . $ip . " : " . $host . ") " . $query . " REFERRER: " . $referrer . " USERAGENT: " . $userAgent;
+
+//CALL OUR LOG FUNCTION
+writeToLogFile($msg);
+
+function writeToLogFile($msg) {
+     $today = date("Y_m_d");
+     $logfile = $today."_log.txt";
+     $dir = 'logs';
+     $saveLocation=$dir . '/' . $logfile;
+     if  (!$handle = @fopen($saveLocation, "a")) {
+	  echo("Not able to open log file\r\n");
+          exit;
+     }
+     else {
+          if (@fwrite($handle,"$msg\r\n") === FALSE) {
+               exit;
+          }
+
+          @fclose($handle);
+     }
+}
+
+// -------------CALL PYTHON SCRIPT -----------------------
     if (strcasecmp($_GET["lang"],"EN") == 0) {
         $lang = "EN";
 	$oppo = "FR";
